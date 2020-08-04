e1bb95c1b8e5e4d493e93da5ff4eb4b3e069b57f
mrichards <mj.richards@viasat.com>
2020-08-04 13:23:25 -0400

    wip

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,15 +15,19 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   onSelected(hero: Hero): void{
     this.selectedHero = hero;
   }
 
   ngOnInit(): void {
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
